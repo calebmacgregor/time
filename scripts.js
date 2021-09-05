d67b92fcdfa23f3058fc3b6be1644e01c715f57e
Caleb MacGregor <calebsmacgregor@gmail.com>
2021-09-05 21:50:53 +1000

    Added various API call functions

--- scripts.js
+++ scripts.js
@@ -1,45 +1,48 @@
 import { getTime, timeCost } from "./calculators.js"
+import { pingUp, getTransactionAccounts, getSaverAccounts } from "./accounts.js"
+
+const token =
+	"up:yeah:NULvDQe7lrgiG9eMgnbP62XrsuUUw2LDkQjN4J3hvFM5w0uTUQnBXtAT8pyjXu6hHk8cduezjrccrgKdRPutlTOX35qdlGJwXZsZCHGYQnAURAbhYSd15buR6v6eAWQr"
+
+const pingURL = "https://api.up.com.au/api/v1/util/ping"
+const accountsURL = "https://api.up.com.au/api/v1/accounts"
+const transactionsURL = "https://api.up.com.au/api/v1/transactions"
+
+const content = document.querySelector("#content")
 
 const rateObject = getTime(2880, 80)
-const expenseItem = timeCost(5, rateObject)
-const token =
-  "up:yeah:NULvDQe7lrgiG9eMgnbP62XrsuUUw2LDkQjN4J3hvFM5w0uTUQnBXtAT8pyjXu6hHk8cduezjrccrgKdRPutlTOX35qdlGJwXZsZCHGYQnAURAbhYSd15buR6v6eAWQr"
+const amountObject = timeCost(5.5, rateObject)
 
+const rent = timeCost(360, rateObject)
 console.log(
-  `That $${expenseItem.amount} item cost you ${expenseItem.hoursPortion} hours, ${expenseItem.minutesPortion} minutes and ${expenseItem.secondsPortion} seconds of your life`
+	`${rent.hoursPortion} hours, ${rent.minutesPortion} minutes, ${rent.secondsPortion} seconds`
 )
 
-let pingURL = "https://api.up.com.au/api/v1/util/ping"
-let accountsURL = "https://api.up.com.au/api/v1/accounts"
+getTransactionAccounts(transactionsURL, token)
 
-// fetch(accountsURL, {
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-// })
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-
-async function getAccounts(url, token) {
-  let response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-  let data = await response.json()
-  return data
+async function getTransactions(url, token) {
+	let response = await fetch(url, {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	})
+	let data = await response.json()
+	return data
 }
 
-let accounts = getAccounts(accountsURL, token)
+getTransactions(transactionsURL, token).then((data) =>
+	data.data.map((transaction) => {
+		let card = {
+			vendor: transaction.attributes.description,
+			datetime: transaction.attributes.settledAt,
+			timeObject: timeCost(
+				Math.abs(transaction.attributes.amount.value),
+				rateObject
+			)
+		}
+		console.log(card)
+		return card
+	})
+)
 
-accounts.then((data) => {
-  data.data.forEach((item) => {
-    console.log(item)
-    console.log(item.attributes.balance.value)
-
-    let expenseItem = timeCost(item.attributes.balance.value, rateObject)
-    console.log(
-      `This account (with $${expenseItem.amount} in it) is worth ${expenseItem.hoursPortion} hours, ${expenseItem.minutesPortion} minutes and ${expenseItem.secondsPortion} seconds`
-    )
-  })
-})
+content.innerText = `That $${amountObject.amount} item cost you ${amountObject.hoursPortion} hours, ${amountObject.minutesPortion} minutes and ${amountObject.secondsPortion} seconds of your life`
