e1837179bea8993dd14a64cb17a06aff935a6255
Caleb MacGregor <calebsmacgregor@gmail.com>
2021-09-06 20:05:21 +1000

    Added in basic pagination testing

--- scripts.js
+++ scripts.js
@@ -9,40 +9,53 @@ const accountsURL = "https://api.up.com.au/api/v1/accounts"
 const transactionsURL = "https://api.up.com.au/api/v1/transactions"
 
 const content = document.querySelector("#content")
+const nextButton = document.querySelector("#next")
+const previousButton = document.querySelector("#previous")
 
 const rateObject = getTime(2880, 80)
 const amountObject = timeCost(5.5, rateObject)
 
-const rent = timeCost(360, rateObject)
-console.log(
-	`${rent.hoursPortion} hours, ${rent.minutesPortion} minutes, ${rent.secondsPortion} seconds`
-)
-
-getTransactionAccounts(transactionsURL, token)
+let nextPage
+let previousPage
 
 async function getTransactions(url, token) {
-	let response = await fetch(url, {
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
+	let data = await pingUp(url, token)
+	nextPage = await data.links.next
+	previousPage = await data.links.prev
+
+	data.data.forEach((transaction) => {
+		let timeCostVariable = timeCost(
+			Math.abs(transaction.attributes.amount.value),
+			rateObject
+		)
+		let datetime = new Date(Date.parse(transaction.attributes.settledAt))
+		console.log(
+			`${datetime.toLocaleString("en-AU")} - ${
+				transaction.attributes.description
+			} - ${timeCostVariable.hoursPortion} hours, ${
+				timeCostVariable.minutesPortion
+			} minutes and ${timeCostVariable.secondsPortion} seconds`
+		)
 	})
-	let data = await response.json()
 	return data
 }
 
-getTransactions(transactionsURL, token).then((data) =>
-	data.data.map((transaction) => {
-		let card = {
-			vendor: transaction.attributes.description,
-			datetime: transaction.attributes.settledAt,
-			timeObject: timeCost(
-				Math.abs(transaction.attributes.amount.value),
-				rateObject
-			)
-		}
-		console.log(card)
-		return card
-	})
-)
+nextButton.addEventListener("click", () => {
+	console.clear()
+	if (!nextPage) {
+		console.log("no next page")
+	} else {
+		getTransactions(nextPage, token)
+	}
+})
 
-content.innerText = `That $${amountObject.amount} item cost you ${amountObject.hoursPortion} hours, ${amountObject.minutesPortion} minutes and ${amountObject.secondsPortion} seconds of your life`
+previousButton.addEventListener("click", () => {
+	console.clear()
+	if (!previousPage) {
+		console.log("no previous page")
+	} else {
+		getTransactions(previousPage, token)
+	}
+})
+
+getTransactions(transactionsURL, token)
