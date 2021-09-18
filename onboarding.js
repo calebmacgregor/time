43103c351998f3dcbe96ab9ddaedaa64b534818e
calebmacgregor <calebsmacgregor@gmail.com>
2021-09-18 16:54:55 +1000

    Deleted utilities, removed inline SVGs and put them into Images folder

--- onboarding.js
+++ onboarding.js
@@ -1,6 +1,7 @@
 import { getTime } from "./calculators.js"
 import { keyValidation } from "./apiCallFunctions.js"
 import { Preferences } from "./classes.js"
+import { pingURL } from "./endpoints.js"
 
 const submit = document.querySelector(".submit")
 const keyInput = document.querySelector(".api-key-input")
@@ -12,76 +13,72 @@ const dollarsError = document.querySelector(".dollars-error")
 const apiError = document.querySelector(".api-error")
 
 //Redirect if the key in storage is valid
-const pingURL = "https://api.up.com.au/api/v1/util/ping"
 let preferences = JSON.parse(localStorage.getItem("TIME-preferences"))
 if (preferences) {
-	let result = await keyValidation(pingURL, preferences.apiKey)
-	if (result.ok == true) {
-		console.log("redirecting")
-		window.location.replace("./")
-	}
+  let result = await keyValidation(pingURL, preferences.apiKey)
+  if (result.ok == true) {
+    console.log("redirecting")
+    window.location.replace("./")
+  }
 }
 
 submit.addEventListener("click", (e) => {
-	e.preventDefault()
+  e.preventDefault()
 
-	//Reset the error messages
-	dollarsError.style.display = "none"
-	timeError.style.display = "none"
-	apiError.style.display = "none"
+  //Reset the error messages
+  dollarsError.style.display = "none"
+  timeError.style.display = "none"
+  apiError.style.display = "none"
 
-	//Grab the values from each input
-	const pay = parseInt(payInput.value)
-	const time = parseInt(timeInput.value)
-	const apiKey = keyInput.value
+  //Grab the values from each input
+  const pay = parseInt(payInput.value)
+  const time = parseInt(timeInput.value)
+  const apiKey = keyInput.value
 
-	//Ping API to seeif key is valid
-	const response = keyValidation(
-		"https://api.up.com.au/api/v1/util/ping",
-		apiKey
-	)
+  //Ping API to seeif key is valid
+  const response = keyValidation(pingURL, apiKey)
 
-	//Check whether numbers are valid
-	const validPay = isNaN(pay) ? false : true
-	const validTime = isNaN(time) ? false : true
+  //Check whether numbers are valid
+  const validPay = isNaN(pay) ? false : true
+  const validTime = isNaN(time) ? false : true
 
-	//Initialise an empty variable to store results of keyValidation
-	let validKey
+  //Initialise an empty variable to store results of keyValidation
+  let validKey
 
-	response.then((data) => {
-		//Set the validity of the validKey
-		//Use this below for error message population
-		if (data.ok) {
-			validKey = true
-		} else {
-			validKey = false
-		}
-		if (data.ok == true && validPay && validTime) {
-			//Construct the preferences object
-			let preferences = new Preferences(
-				apiKey,
-				true,
-				pay,
-				time,
-				getTime(pay, time)
-			)
+  response.then((data) => {
+    //Set the validity of the validKey
+    //Use this below for error message population
+    if (data.ok) {
+      validKey = true
+    } else {
+      validKey = false
+    }
+    if (data.ok == true && validPay && validTime) {
+      //Construct the preferences object
+      let preferences = new Preferences(
+        apiKey,
+        true,
+        pay,
+        time,
+        getTime(pay, time)
+      )
 
-			//Put the preferences object into localstorage
-			localStorage.setItem("TIME-preferences", JSON.stringify(preferences))
+      //Put the preferences object into localstorage
+      localStorage.setItem("TIME-preferences", JSON.stringify(preferences))
 
-			//Redirect to the main page
-			window.location.replace("./")
-		} else {
-			if (!validPay) {
-				dollarsError.style.display = "block"
-			}
-			if (!validTime) {
-				timeError.style.display = "block"
-			}
+      //Redirect to the main page
+      window.location.replace("./")
+    } else {
+      if (!validPay) {
+        dollarsError.style.display = "block"
+      }
+      if (!validTime) {
+        timeError.style.display = "block"
+      }
 
-			if (!validKey) {
-				apiError.style.display = "block"
-			}
-		}
-	})
+      if (!validKey) {
+        apiError.style.display = "block"
+      }
+    }
+  })
 })
--- scripts.js
+++ scripts.js
@@ -1,20 +1,14 @@
 import { getTime, timeValue } from "./calculators.js"
 import { renderTransactions, renderBalance } from "./render.js"
 import {
-	pingUp,
-	getTransactionAccounts,
-	getSaverAccounts,
-	getAccounts,
-	keyValidation
+  pingUp,
+  getTransactionAccounts,
+  getSaverAccounts,
+  getAccounts,
+  keyValidation,
 } from "./apiCallFunctions.js"
 import { timeTransaction } from "./classes.js"
-
-//Add
-
-const pingURL = "https://api.up.com.au/api/v1/util/ping"
-const accountsURL = "https://api.up.com.au/api/v1/accounts"
-const transactionsURL =
-	"https://api.up.com.au/api/v1/transactions?page[size]=50"
+import { pingURL, accountsURL, transactionsURL } from "./endpoints.js"
 
 //Set some global variables
 let nextPage
@@ -22,12 +16,12 @@ let preferences = JSON.parse(localStorage.getItem("TIME-preferences"))
 
 //Redirect if the key in storage is bad
 if (!preferences) {
-	window.location.replace("./onboarding.html")
+  window.location.replace("./onboarding.html")
 } else if (preferences) {
-	let result = await keyValidation(pingURL, preferences.apiKey)
-	if (result.ok == false) {
-		window.location.replace("./onboarding.html")
-	}
+  let result = await keyValidation(pingURL, preferences.apiKey)
+  if (result.ok == false) {
+    window.location.replace("./onboarding.html")
+  }
 }
 
 const rateObject = preferences.rateObject
@@ -36,104 +30,104 @@ const rateObject = preferences.rateObject
 renderBalance(getBalance(accountsURL, preferences.apiKey, rateObject))
 
 renderTransactions(
-	getTransactions(transactionsURL, preferences.apiKey, timeValue),
-	preferences
+  getTransactions(transactionsURL, preferences.apiKey, timeValue),
+  preferences
 )
 
 //Get transactions from Up and run them through
 //the timevalue function
 export async function getTransactions(url, token) {
-	let data = await pingUp(url, token)
-	nextPage = await data.links.next
-	let output = await data.data.map((item) => {
-		return new timeTransaction(item, rateObject, timeValue)
-	})
-	return output
+  let data = await pingUp(url, token)
+  nextPage = await data.links.next
+  let output = await data.data.map((item) => {
+    return new timeTransaction(item, rateObject, timeValue)
+  })
+  return output
 }
 
 async function getBalance(accountsURL, token, rateObject) {
-	const transactionAccount = await getTransactionAccounts(accountsURL, token)
-	const transactionBalance = await transactionAccount.reduce((total, item) => {
-		return total + parseFloat(item.attributes.balance.value)
-	}, 0)
+  const transactionAccount = await getTransactionAccounts(accountsURL, token)
+  const transactionBalance = await transactionAccount.reduce((total, item) => {
+    return total + parseFloat(item.attributes.balance.value)
+  }, 0)
 
-	const accounts = await getAccounts(accountsURL, token)
-	const accountsBalance = await accounts.reduce((total, item) => {
-		return total + parseFloat(item.attributes.balance.value)
-	}, 0)
+  const accounts = await getAccounts(accountsURL, token)
+  const accountsBalance = await accounts.reduce((total, item) => {
+    return total + parseFloat(item.attributes.balance.value)
+  }, 0)
 
-	const savingsAccounts = await getAccounts(accountsURL, token)
-	const savingsBalance = await savingsAccounts.reduce((total, item) => {
-		return total + parseFloat(item.attributes.balance.value)
-	}, 0)
+  const savingsAccounts = await getAccounts(accountsURL, token)
+  const savingsBalance = await savingsAccounts.reduce((total, item) => {
+    return total + parseFloat(item.attributes.balance.value)
+  }, 0)
 
-	const balanceTimeValue = timeValue(accountsBalance, rateObject)
-	const transactionBalanceTimeValue = timeValue(transactionBalance, rateObject)
-	return {
-		balanceTimeValue: balanceTimeValue,
-		transactionBalanceTimeValue: transactionBalanceTimeValue
-	}
+  const balanceTimeValue = timeValue(accountsBalance, rateObject)
+  const transactionBalanceTimeValue = timeValue(transactionBalance, rateObject)
+  return {
+    balanceTimeValue: balanceTimeValue,
+    transactionBalanceTimeValue: transactionBalanceTimeValue,
+  }
 }
 
 //Infinite scrolling logic
 function infiniteScroll() {
-	let scrollLocation = window.innerHeight + window.pageYOffset
-	let scrollHeight = document.body.scrollHeight
+  let scrollLocation = window.innerHeight + window.pageYOffset
+  let scrollHeight = document.body.scrollHeight
 
-	if (scrollLocation / scrollHeight > 0.8) {
-		if (!nextPage) {
-			console.log("no next page")
-		} else {
-			console.log("Grabbing new transactions")
-			renderTransactions(
-				getTransactions(nextPage, preferences.apiKey, timeValue),
-				preferences
-			)
-		}
-	}
+  if (scrollLocation / scrollHeight > 0.8) {
+    if (!nextPage) {
+      console.log("no next page")
+    } else {
+      console.log("Grabbing new transactions")
+      renderTransactions(
+        getTransactions(nextPage, preferences.apiKey, timeValue),
+        preferences
+      )
+    }
+  }
 }
 
 //Set event handlers on touchstart and touchend
 //This helps reduce the number of calls compared
 //to using scroll
 document.addEventListener("touchend", () => {
-	infiniteScroll()
+  infiniteScroll()
 })
 
 document.addEventListener("touchstart", () => {
-	infiniteScroll()
+  infiniteScroll()
 })
 
 //Toggle between transactional and total views for balance
 document.addEventListener("click", (e) => {
-	if (!e.target.classList.contains("balance")) return
-	e.target.classList.toggle("total")
-	renderBalance(getBalance(accountsURL, preferences.apiKey, rateObject))
+  if (!e.target.classList.contains("balance")) return
+  e.target.classList.toggle("total")
+  renderBalance(getBalance(accountsURL, preferences.apiKey, rateObject))
 })
 
 //Fade and scale down the balance on scroll
 document.addEventListener("scroll", (e) => {
-	const balanceContainer = document.querySelector(".balance-container")
-	const balanceContainerHeight = balanceContainer.offsetHeight
-	let scrollLocation = window.pageYOffset
+  const balanceContainer = document.querySelector(".balance-container")
+  const balanceContainerHeight = balanceContainer.offsetHeight
+  let scrollLocation = window.pageYOffset
 
-	let percentVisibile = 1 - (scrollLocation * 1.5) / balanceContainerHeight
+  let percentVisibile = 1 - (scrollLocation * 1.5) / balanceContainerHeight
 
-	percentVisibile > 0
-		? (balanceContainer.style.opacity = percentVisibile)
-		: (balanceContainer.style.opacity = 0)
+  percentVisibile > 0
+    ? (balanceContainer.style.opacity = percentVisibile)
+    : (balanceContainer.style.opacity = 0)
 })
 
 //Switches transactions between dollar and time views (time is default)
 document.addEventListener("click", (e) => {
-	if (!e.target.classList.contains("transaction-related")) return
-	const transaction = e.target.closest(".transaction")
+  if (!e.target.classList.contains("transaction-related")) return
+  const transaction = e.target.closest(".transaction")
 
-	const timeSpent = transaction.querySelector(".time-spent")
-	const moneySpent = transaction.querySelector(".money-spent")
+  const timeSpent = transaction.querySelector(".time-spent")
+  const moneySpent = transaction.querySelector(".money-spent")
 
-	timeSpent.classList.toggle("hidden")
-	moneySpent.classList.toggle("hidden")
+  timeSpent.classList.toggle("hidden")
+  moneySpent.classList.toggle("hidden")
 })
 
 //Handle calculating a time value on demand
@@ -141,24 +135,24 @@ const estimateInput = document.querySelector(".estimate-input")
 const estimateResult = document.querySelector(".estimate-result")
 
 estimateInput.addEventListener("input", () => {
-	if (!isNaN(estimateInput.value)) {
-		const timeValueObject = timeValue(
-			estimateInput.value,
-			preferences.rateObject
-		)
+  if (!isNaN(estimateInput.value)) {
+    const timeValueObject = timeValue(
+      estimateInput.value,
+      preferences.rateObject
+    )
 
-		const timeValueString = `${
-			timeValueObject.hoursPortion < 10
-				? "0" + timeValueObject.hoursPortion
-				: timeValueObject.hoursPortion
-		}h:${
-			timeValueObject.minutesPortion < 10
-				? "0" + timeValueObject.minutesPortion
-				: timeValueObject.minutesPortion
-		}m`
+    const timeValueString = `${
+      timeValueObject.hoursPortion < 10
+        ? "0" + timeValueObject.hoursPortion
+        : timeValueObject.hoursPortion
+    }h:${
+      timeValueObject.minutesPortion < 10
+        ? "0" + timeValueObject.minutesPortion
+        : timeValueObject.minutesPortion
+    }m`
 
-		estimateResult.innerText = timeValueString
-	}
+    estimateResult.innerText = timeValueString
+  }
 })
 
 //open estimate panel functionality
@@ -166,22 +160,22 @@ const estimatePanel = document.querySelector(".estimate-panel")
 const closeButton = document.querySelector(".close-button")
 
 closeButton.addEventListener("click", () => {
-	//If you click the button when the panel is closed
-	if (estimatePanel.classList.contains("hidden")) {
-		closeButton.classList.remove("maximise")
-		estimatePanel.style.display = "flex"
-		setTimeout(() => {
-			estimatePanel.classList.remove("hidden")
-		}, 5)
-	}
-	//If you click the button when the panel is open
-	else {
-		closeButton.classList.add("maximise")
-		estimatePanel.classList.toggle("hidden")
-		setTimeout(() => {
-			estimatePanel.style.display = "none"
-		}, 251)
-	}
+  //If you click the button when the panel is closed
+  if (estimatePanel.classList.contains("hidden")) {
+    closeButton.classList.remove("maximise")
+    estimatePanel.style.display = "flex"
+    setTimeout(() => {
+      estimatePanel.classList.remove("hidden")
+    }, 5)
+  }
+  //If you click the button when the panel is open
+  else {
+    closeButton.classList.add("maximise")
+    estimatePanel.classList.toggle("hidden")
+    setTimeout(() => {
+      estimatePanel.style.display = "none"
+    }, 251)
+  }
 })
 
 //Open nav panel functionality
@@ -189,39 +183,39 @@ const navIcon = document.querySelector(".nav-icon")
 const navPanel = document.querySelector(".nav-panel")
 
 navIcon.addEventListener("click", () => {
-	navPanel.classList.toggle("hidden")
-	navIcon.classList.toggle("expanded")
-	if (!navPanel.classList.contains("hidden")) {
-		navIcon.style.zIndex = 1000
-	} else {
-		navIcon.style.zIndex = 2
-	}
+  navPanel.classList.toggle("hidden")
+  navIcon.classList.toggle("expanded")
+  if (!navPanel.classList.contains("hidden")) {
+    navIcon.style.zIndex = 1000
+  } else {
+    navIcon.style.zIndex = 2
+  }
 })
 
 //Disable scroll when panels are open
 document.addEventListener(
-	"touchmove",
-	(e) => {
-		if (estimatePanel.classList.contains("hidden")) return
-		e.preventDefault()
-	},
-	{ passive: false }
+  "touchmove",
+  (e) => {
+    if (estimatePanel.classList.contains("hidden")) return
+    e.preventDefault()
+  },
+  { passive: false }
 )
 
 document.addEventListener(
-	"touchmove",
-	(e) => {
-		if (navPanel.classList.contains("hidden")) return
-		e.preventDefault()
-	},
-	{ passive: false }
+  "touchmove",
+  (e) => {
+    if (navPanel.classList.contains("hidden")) return
+    e.preventDefault()
+  },
+  { passive: false }
 )
 
 //Logout functionality
 const logout = document.querySelector(".logout")
 
 logout.addEventListener("click", (e) => {
-	localStorage.removeItem("TIME-preferences")
-	preferences = ""
-	location.reload()
+  localStorage.removeItem("TIME-preferences")
+  preferences = ""
+  location.reload()
 })
